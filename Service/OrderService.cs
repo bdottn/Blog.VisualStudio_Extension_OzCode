6ac2327438569ae9af0f688bd21a906c2d3c25a1
Echo Lee <bdottn.lee+git@gmail.com>
2017-05-27 01:58:57 +0800

    建立 OrderService
    1.　加入方法：DailySales

---
+++ Service/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Service
+{
+    public sealed class OrderService
+    {
+        public List<Order> FakeOrders { get; set; }
+
+        public int DailySales(DateTime date)
+        {
+            var dailyOrders = this.GetOrders(date);
+
+            var completeOrders = this.FilterOrders(dailyOrders, OrderStatus.Complete);
+
+            var dailySales = this.CalculatorOrderAmount(completeOrders);
+
+            return dailySales;
+        }
+
+        private IEnumerable<Order> GetOrders(DateTime date)
+        {
+            return this.FakeOrders.Where(o => o.Date == date.Date);
+        }
+
+        private IEnumerable<Order> FilterOrders(IEnumerable<Order> orders, OrderStatus status)
+        {
+            return orders.Where(o => o.Status == status);
+        }
+
+        private int CalculatorOrderAmount(IEnumerable<Order> orders)
+        {
+            return orders.Sum(o => this.CalculatorOrderAmount(o));
+        }
+
+        private int CalculatorOrderAmount(Order order)
+        {
+            return order.Detail.Sum(g => g.Price);
+        }
+    }
+}
