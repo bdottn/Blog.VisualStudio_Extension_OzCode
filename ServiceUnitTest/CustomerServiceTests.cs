fb4d2bf6a321a219a722a161bfbb82b2039f3724
Echo Lee <bdottn.lee+git@gmail.com>
2017-05-27 02:18:30 +0800

    建立 CustomerServiceTests 單元測試
    1.　加入測試案例：TotalCostTest_傳入Customer_訂單為null_預期回傳Exception

---
+++ ServiceUnitTest/CustomerServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+
+namespace Service.UnitTest
+{
+    [TestClass]
+    public sealed class CustomerServiceTests
+    {
+        private CustomerService service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.service = new CustomerService();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TotalCostTest_傳入Customer_訂單為null_預期回傳Exception()
+        {
+            this.service.FakeCustomer =
+                new Customer()
+                {
+                    Name = "高級書僮",
+                    Orders = null,
+                };
+
+            this.service.TotalCost();
+        }
+    }
+}
